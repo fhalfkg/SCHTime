37dd857356cbaeaba1fe6d0d4da0d46ee6676693
Park-jaehyeon02 <1234qkr@gmail.com>
2022-11-02 11:39:43 +0900

    창크기 설정

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import tkinter
 
 win = tkinter.Tk()
+win.geometry("300x300+500+140")
 
 btn1 = tkinter.Button(win,text="btn1")
-btn1.config(width=10)
+btn1.config(width=50)
 btn1.pack()
 
 btn2 = tkinter.Button(win,text="btn2")
-btn2.config(width=10)
+btn2.config(width=50)
 btn2.pack()
 
 btn3 = tkinter.Button(win,text="btn3")
-btn3.config(width=10)
+btn3.config(width=50)
 btn3.pack()
 
 win.mainloop()
