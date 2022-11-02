23f68166d8b355ae57823e8629ad0341a6e87007
Park-jaehyeon02 <1234qkr@gmail.com>
2022-11-02 11:34:40 +0900

    기본 메인 화면 생성

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter
+
+win = tkinter.Tk()
+
+btn1 = tkinter.Button(win,text="btn1")
+btn1.config(width=10)
+btn1.pack()
+
+btn2 = tkinter.Button(win,text="btn2")
+btn2.config(width=10)
+btn2.pack()
+
+btn3 = tkinter.Button(win,text="btn3")
+btn3.config(width=10)
+btn3.pack()
+
+win.mainloop()
