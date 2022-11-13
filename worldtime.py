fb9c4b0f5b3b7a3c78cf0c0b80bcaaf7aab563f5
Park-jaehyeon02 <1234qkr@gmail.com>
2022-11-14 00:19:27 +0900

    시간 실시간 변화 작동

--- worldtime.py
+++ worldtime.py
@@ -1,12 +1,21 @@
 import tkinter
-
+import time
 
 class WorldTimePage(tkinter.Tk):
     def __init__(self, parent):
         tkinter.Tk.__init__(self, parent)
 
+        self.clockListName = []
+        self.clockListTime = []
+
         self.parent = parent
         self.initialize()
+
+    def clock(self,live_T = time.strftime("%H:%M:%S")):
+        live_T = time.strftime("%H:%M:%S")
+        self.clockListTime[0].config(text=live_T)
+        self.clockListTime[0].after(1,self.clock)
+        
 
     def initialize(self):
         # set window env
@@ -16,19 +25,31 @@ class WorldTimePage(tkinter.Tk):
         self.geometry("300x320+800+140")
         self.title("세계시간")
 
-        # TODO: 동적으로 시간 표시
-        WorldTimeListBuilder.build(self, [
+        
+
+        time_list = [
             {
                 "name": "서울",
-                "time": "2019-01-01 00:00:00"
-            }
-        ])
+                #"time": "2019-01-01 00:00:00"
+                "time" :time.strftime("%H:%M:%S")
+            },
+            {
+                "name": "서울2",
+                #"time": "2019-01-01 00:00:00"
+                "time" :time.strftime("%H:%M:%S")
+            },
+        ]
+        
+        self.build(time_list)
 
+    # TODO: 동적으로 시간 표시
 
-class WorldTimeListBuilder():
     def build(self, timelist):
         for i, data in enumerate(timelist):
-            tkinter.Label(self, text=data["name"]).grid(
-                column=0, row=i, sticky="ew")
-            tkinter.Label(self, text=data["time"]).grid(
-                column=1, row=i, sticky="ew")
+            a=tkinter.Label(self, text=data["name"])
+            a.grid(column=0, row=i, sticky="ew")
+            b=tkinter.Label(self, text=data["time"])
+            b.grid(column=1, row=i, sticky="ew")
+            self.clockListName.append(a)
+            self.clockListTime.append(b)
+        self.clock()
